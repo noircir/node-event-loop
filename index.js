4acb70e36878eca982ee6cc163c923fc1f2a567c
Nellie <elenagithub@gmail.com>
2019-02-21 18:02:56 -0500

    benchmarking forks and CPUs.... ab -c 2 -n 2 localhost:3000/

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 // Is the file going to be executed in master mode?
 if (cluster.isMaster) {
 	// Cause index.js be executed *again* but in child mode
 	cluster.fork();
-	// cluster.fork();
-	// cluster.fork();
-	// cluster.fork();
+	cluster.fork();
 } else {
 	// I'm a child, I'm going to act like a server, and nothing else
 	const express = require('express');
+	const crypto = require('crypto');
 	const app = express();
 
-	function doWork(duration) {
-		const start = Date.now();
-		while (Date.now() - start < duration) {}
-	}
+	// function doWork(duration) {
+	// 	const start = Date.now();
+	// 	while (Date.now() - start < duration) {}
+	// }
 
 	app.get('/', (req, res) => {
-		doWork(5000);
-		res.send('Hi there');
+		// doWork(5000);
+		crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+			res.send('Hi there');
+		});
 	});
 
 	app.get('/fast', (req, res) => {
